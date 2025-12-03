e5f9f8560545986c03cd3003e6ec3b6e53e91f1f
Aasim-Qureshi <aasimq194@gmail.com>
2025-12-03 15:55:49 +0530

    Fixed Fill

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const bodyParser = require('express').json;
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser')
-
+const cookieParser = require('cookie-parser');
 
 const routes = require('./presentation/routes/index.routes');
 
-
 const app = express();
 
-app.use(cookieParser())
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({
+    limit: '100mb',
+    extended: true
+}));
+app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(bodyParser());
-
 
 app.use('/api', routes);
 app.get('/', (req, res) => res.json({ message: 'Hello World' }));
