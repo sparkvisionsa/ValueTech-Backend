ac4e815b9dc6b68b998e349bab0ed38d50bbcc92
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-12-23 11:58:59 +0300

    Addede Model src/infrastructure/models/MultiApproachReport.js with Validations

--- src/infrastructure/models/MultiApproachReport.js
+++ src/infrastructure/models/MultiApproachReport.js
@@ -184,7 +184,7 @@ function toYyyyMmDd(value) {
   if (value instanceof Date) {
     const yyyy = value.getFullYear();
     const mm = String(value.getMonth() + 1).padStart(2, "0");
-    const dd = String(value.getDate()).padStart(2, "0");
+    const dd = String(value.getDate() + 1).padStart(2, "0");
     return `${yyyy}-${mm}-${dd}`;
   }
   // Assume it's already a string, just return it
